27663e1d664b6b991503d9700a57df8c18ca9ae1
hashfx <algebra.soni@gmail.com>
2022-09-27 16:48:49 +0530

    intl package && clock UI {title + time + day + date + timeZone}

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import 'clock_view.dart';
 
@@ -13,49 +14,63 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     var now = DateTime.now();
+    var formattedTime = DateFormat('HH:mm').format(now);
+    var formattedDate = DateFormat('EEE, d MMM').format(now);
+    var timeZoneString = now.timeZoneOffset.toString().split('.').first;
+    // timeZoneOffset is returned either -ve or +ve
+    var offsetSign = '';
+    // if timeZone not starts with -, make it +
+    if (!timeZoneString.startsWith('-')) offsetSign = '+';
+
+    print(timeZoneString);
+
     return Scaffold(
       backgroundColor: Color(0xFF202F41),
-      body: Container(
-        padding: EdgeInsets.all(32),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              'Clock',
-              style: TextStyle(color: Colors.white, fontSize: 24),
-            ),
-            SizedBox(height: 32),
-            Text(
-              'Clock',
-              style: TextStyle(color: Colors.white, fontSize: 64),
-            ),
-            Text(
-              'Clock',
-              style: TextStyle(color: Colors.white, fontSize: 20),
-            ),
-            ClockView(),
-            Text(
-              'Time Zone',
-              style: TextStyle(color: Colors.white, fontSize: 24),
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            Row(
+      body: Row(
+        children: [
+          Container(
+            padding: EdgeInsets.all(32),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Icon(
-                  Icons.language,
-                  color: Colors.white,
-                ),
-                SizedBox(width: 16),
                 Text(
-                  'IST',
+                  'Clock',
                   style: TextStyle(color: Colors.white, fontSize: 24),
                 ),
+                SizedBox(height: 32),
+                Text(
+                  formattedTime,
+                  style: TextStyle(color: Colors.white, fontSize: 64),
+                ),
+                Text(
+                  formattedDate,
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+                ClockView(),
+                Text(
+                  'Time Zone',
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.language,
+                      color: Colors.white,
+                    ),
+                    SizedBox(width: 16),
+                    Text(
+                      'UTC ' + offsetSign + timeZoneString,
+                      style: TextStyle(color: Colors.white, fontSize: 14),
+                    ),
+                  ],
+                )
               ],
-            )
-          ],
-        ),
+            ),
+          ),
+        ],
       ),
     );
   }
