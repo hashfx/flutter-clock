1c8ff33fabaf0e17b98577fa75bd139288babd5b
hashfx <algebra.soni@gmail.com>
2022-09-27 15:18:22 +0530

    mathematical logic to rotate hands w.r.t. current time

--- lib/clock_view.dart
+++ lib/clock_view.dart
@@ -15,15 +15,17 @@ class _ClockViewState extends State<ClockView> {
     return Container(
         width: 300,
         height: 300,
-        child: CustomPaint(
-          painter: ClockPainter(),
+        child: Transform.rotate(
+          angle: -pi / 2, // rotate canvas by deg90 anti-clockwise
+          child: CustomPaint(
+            painter: ClockPainter(),
+          ),
         ));
   }
 }
 
 class ClockPainter extends CustomPainter {
-  var dateTime = DateTime.now();  // dateTime object gives current date and time
-  
+  var dateTime = DateTime.now(); // dateTime object gives current date and time
 
   // implement virtual and abstract methods
 
@@ -81,18 +83,20 @@ class ClockPainter extends CustomPainter {
     // draw hands of clock
 
     // automate hour hand of clock :: for radius, find a point on circumference
-    var hourHandX = centerX + 60 * cos(180 * pi / 180);
-    var hourHandY = centerX + 60 * sin(180 * pi / 180);
+    var hourHandX = centerX +
+        60 * cos((dateTime.hour * 30 + dateTime.minute * 0.5) * pi / 180);
+    var hourHandY = centerX +
+        60 * sin((dateTime.hour * 30 + dateTime.minute * 0.5) * pi / 180);
     canvas.drawLine(center, Offset(hourHandX, hourHandY), hourHandBrush);
 
 // automate minute hand of clock :: for radius, find a point on circumference
-    var minHandX = centerX + 80 * cos(45 * pi / 180);
-    var minHandY = centerX + 80 * sin(45 * pi / 180);
+    var minHandX = centerX + 80 * cos(dateTime.minute * 6 * pi / 180);
+    var minHandY = centerX + 80 * sin(dateTime.minute * 6 * pi / 180);
     canvas.drawLine(center, Offset(minHandX, minHandY), minHandBrush);
 
     // automate second hand of clock :: for radius, find a point on circumference
-    var secHandX = centerX + 80 * cos(90 * pi / 180);
-    var secHandY = centerX + 80 * sin(90 * pi / 180);
+    var secHandX = centerX + 80 * cos(dateTime.second * 6 * pi / 180);
+    var secHandY = centerX + 80 * sin(dateTime.second * 6 * pi / 180);
     canvas.drawLine(center, Offset(secHandX, secHandY), secHandBrush);
 
     // center dot of clock
--- structure.md
+++ structure.md
@@ -5,5 +5,7 @@ define clock in clock_view.dart file
     class ClockPainter extends CustomPainter
         override paint() and shouldRepaint() methods
         drawCircle(center, radius, brush)
-        // 60 sec = deg360 -> 1 sec = deg6
+        // second: 60 sec = deg360 -> 1 sec = deg6
+        // minute: 60 min = deg360 -> 1 min = deg6
+        // hour: 12hr = deg360 -> 1 hr = deg30 :: deg0.5 in 1 min
 ```
