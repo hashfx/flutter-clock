06f91f814b4e9c3e41a39f53bcac24441bea491c
hashfx <algebra.soni@gmail.com>
2022-10-04 13:02:20 +0530

    enums for menubar && added provider

---
+++ lib/menu_info.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_clock/enums.dart';
+
+class MenuInfo {
+  MenuType menuType;
+  String title;
+  String imageSource;
+
+  MenuInfo(this.menuType, {required this.title, required this.imageSource});
+}
